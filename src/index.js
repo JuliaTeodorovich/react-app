b99b348e586f3af99928632de42765e8b3350560
JuliaTeo <teoyulia@gmail.com>
2023-10-25 19:44:17 +0300

    task 7

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import AppRouter from "./router";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppRouter/>
+    <AppRouter />
   </React.StrictMode>
 );
